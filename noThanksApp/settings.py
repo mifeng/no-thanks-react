0f73b9677d0b6dc8f3612538829f7dcc4b7bf324
Yzhang1994 <yzhang1994@gmail.com>
2017-08-11 22:42:25 -0700

    try again

--- noThanksApp/settings.py
+++ noThanksApp/settings.py
@@ -107,7 +107,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     (os.path.join(BASE_DIR, 'static')),
