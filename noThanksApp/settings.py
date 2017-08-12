934f02dd488cf870d7debb9f79e31eb2351baf96
Yzhang1994 <yzhang1994@gmail.com>
2017-08-11 23:27:26 -0700

    ijw

--- noThanksApp/settings.py
+++ noThanksApp/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'rest_framework',
     'channels',
