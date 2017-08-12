d852e2fb59ee3f2b1b029754e57bfff91c2bc09e
Yzhang1994 <yzhang1994@gmail.com>
2017-08-12 00:29:14 -0700

    revert

--- noThanksApp/settings.py
+++ noThanksApp/settings.py
@@ -111,7 +111,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     (os.path.join(BASE_DIR, 'static')),
-    (os.path.join(BASE_DIR, 'templates')),
 ]
 
 # Simplified static file serving.
